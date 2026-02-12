d29d9e81dbfe8d32048dedbfbf4751083049cb3f
Carlos Navajas <navahas@culturecode.dev>
2026-02-12 13:37:56 +0100

    feat: c rocket implementation

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 
 // The Ghosts
@@ -5,7 +6,48 @@ struct Idle;
 struct Armed;
 struct Launched;
 
+#define DEFINE_ROCKET(TAG)                                                     \
+    struct RocketState##TAG {                                                  \
+        int id;                                                                \
+        struct TAG *_phantom;                                                  \
+    };
+
+DEFINE_ROCKET(Idle);
+DEFINE_ROCKET(Armed);
+DEFINE_ROCKET(Launched);
+
+typedef struct RocketStateIdle rocket_idle;
+typedef struct RocketStateArmed rocket_armed;
+typedef struct RocketStateLaunched rocket_launched;
+
+rocket_idle rocket_new(int id) {
+    rocket_idle r = {.id = id, ._phantom = NULL};
+    printf("[rocket_new] id: %d | _phantom: %p\n", r.id, r._phantom);
+    return r;
+}
+
+rocket_armed rocket_arm(rocket_idle r) {
+    rocket_armed r_new = {.id = r.id, ._phantom = NULL};
+    printf("[rocket_arm] id: %d | _phantom: %p\n", r.id, r._phantom);
+    return r_new;
+}
+
+rocket_launched rocket_launch(rocket_armed r) {
+    rocket_launched r_new = {.id = r.id, ._phantom = NULL};
+    printf("[rocket_launch] id: %d | _phantom: %p\n", r.id, r._phantom);
+    return r_new;
+}
+
+rocket_idle rocket_reset(rocket_launched r) {
+    rocket_idle r_new = {.id = r.id, ._phantom = NULL};
+    printf("[rocket_reset] id: %d | _phantom: %p\n", r.id, r._phantom);
+    return r_new;
+}
+
 int main() {
-	printf("hello, world!\n");
-	return 0;
+    rocket_idle r1 = rocket_new(11);
+    rocket_armed r2 = rocket_arm(r1);
+    rocket_launched r3 = rocket_launch(r2);
+    rocket_reset(r3);
+    return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -4,49 +4,59 @@ use core::marker::PhantomData;
 
 #[derive(Debug, Copy, Clone)]
 struct Idle;
+#[derive(Debug, Copy, Clone)]
 struct Armed;
+#[derive(Debug, Copy, Clone)]
 struct Launched;
 
 #[derive(Debug, Copy, Clone)]
 struct Rocket<State> {
     id: i32,
-    _marker: PhantomData<State>
+    _marker: PhantomData<State>,
 }
 
 impl Rocket<Idle> {
-    pub fn new() -> Self {
-        Self {
-            id: 12345,
-            _marker: PhantomData
-        }
+    pub fn new(id: i32) -> Self {
+        let _rocket = Self {
+            id,
+            _marker: PhantomData,
+        };
+        println!("[rocket::new] -> {:?}", _rocket);
+        return _rocket;
     }
     pub fn arm(self) -> Rocket<Armed> {
-        Rocket {
+        let _rocket = Rocket {
             id: self.id,
-            _marker: PhantomData
-        }
+            _marker: PhantomData,
+        };
+        println!("[rocket::arm] -> {:?}", _rocket);
+        return _rocket;
     }
 }
 
 impl Rocket<Armed> {
     pub fn launch(self) -> Rocket<Launched> {
-        Rocket {
+        let _rocket = Rocket {
             id: self.id,
-            _marker: PhantomData
-        }
+            _marker: PhantomData,
+        };
+        println!("[rocket::launch] -> {:?}", _rocket);
+        return _rocket;
     }
 }
 
 impl Rocket<Launched> {
     pub fn reset(self) -> Rocket<Idle> {
-        Rocket {
+        let _rocket = Rocket {
             id: self.id,
-            _marker: PhantomData
-        }
+            _marker: PhantomData,
+        };
+        println!("[rocket::reset] -> {:?}", _rocket);
+        return _rocket;
     }
 }
 
 fn main() {
-    let rocket = Rocket::new();
+    let rocket = Rocket::new(111);
     rocket.arm().launch().reset();
 }
